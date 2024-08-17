7085a74cf3dd21097f64d8beadc11319d09da0a2
jonathan03ant <jonathan03ant@gmail.com>
2024-08-17 20:54:17 +0000

    accessing tokens

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 import requests
 from dotenv import load_dotenv
 import os
+import base64
+from requests import post
 
 load_dotenv()
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 
-print(client_id)
-print(client_secret)
+def get_token():
+    # To get a token, we need to send a POST request to the Spotify API
+        # We want to include the following in our request:
+            # URL: https://accounts.spotify.com/api/token
+            # Headers:
+                # Content-Type: application/x-www-form-urlencoded
+                # Authorization: Basic <base64 encoded client_id:client_secret>
+            # Data:
+                # grant_type: client_credentials
+ 
+    url = "https://accounts.spotify.com/api/token"
+    header = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
+    
+    response = requests.post(url,
+                             headers = {
+                                    "Content-Type": "application/x-www-form-urlencoded",
+                                    "Authorization": f"Basic {header}"
+                             },
+                             data = {
+                                    "grant_type": "client_credentials"
+                             })
+    
+    token = response.json().get("access_token")
+    return token
+
+
+
+token = get_token()
+print(token)
+    
+    
+    
+    
+
+    
